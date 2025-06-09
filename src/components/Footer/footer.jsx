1351d4354c658e3bac181495c27274e502f93072
Vipin Nagar <100818576+vipinog@users.noreply.github.com>
2025-06-09 13:06:07 +0530

    Update footer.jsx
    commited the changes in commitment .    LOL!!

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -30,7 +30,7 @@ function Footer() {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400 font-bold ">
-            BattleSecore is aspiring to become Google of esports.
+            BattleSecore is Directory to BGMI & PUBG MOBILE.
           </span>
         </div>
       </footer>
